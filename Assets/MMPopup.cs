e948146c360386511359529e94ef3a6a8bae9bea
pivarcsi.gergely <gergely.pivarcsi@gmail.com>
2021-12-19 17:05:55 +0100

    Countdown popup window whenever you hit Matchmake

--- Assets/MMPopup.cs
+++ Assets/MMPopup.cs
@@ -1,26 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
-public class MMPopupWindow
-{
-    private string title;
-    private string message;
-}
 
 public class MMPopup : MonoBehaviour
 {
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI messageText;
 }
